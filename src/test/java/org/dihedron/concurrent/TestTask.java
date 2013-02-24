d2c562855c01b0be1fa5137684abe94a89d4afc4
Andrea Funtò <dihedron.dev@gmail.com>
2013-02-24 17:45:30 +0100

    Initial commit

---
+++ src/test/java/org/dihedron/concurrent/TestTask.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2013, Andrea Funto'. All rights reserved.
+ * 
+ * This file is part of the Dihedron Common Utilities library ("Commons").
+ *
+ * "Commons" is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU Lesser General Public License as published by the Free 
+ * Software Foundation, either version 3 of the License, or (at your option) 
+ * any later version.
+ *
+ * "Commons" is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with "Commons". If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.dihedron.concurrent;
+
+
+/**
+ * @author Andrea Funto'
+ */
+public class TestTask extends Task<String> {
+
+	private long sleepTime;
+	
+	public TestTask(long sleepTime) {
+		this.sleepTime = sleepTime;
+	}
+	
+	/**
+	 * @see org.dihedron.concurrent.Task#execute()
+	 */
+	@Override
+	protected String execute() throws Exception {
+        Thread.sleep(sleepTime);
+        return "task complete after having slept for " + sleepTime + " ms";
+	}
+}
