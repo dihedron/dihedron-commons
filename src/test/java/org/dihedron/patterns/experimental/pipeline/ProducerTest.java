e1eb3f1ce3ce7ea6795e3e8a43303a2ae15325a1
Andrea Funtò <dihedron.dev@gmail.com>
2014-10-24 08:26:22 +0200

    Updated the license in all files, with a reference to the external LICENSE file.

--- src/test/java/org/dihedron/patterns/experimental/pipeline/ProducerTest.java
+++ src/test/java/org/dihedron/patterns/experimental/pipeline/ProducerTest.java
@@ -1,28 +1,8 @@
 /**
- * Copyright (c) 2012-2014, Andrea Funto'. All rights reserved.
- * 
- * This file is part of the Dihedron Common Utilities library ("Commons").
- *
- * "Commons" is free software: you can redistribute it and/or modify it under 
- * the terms of the GNU Lesser General Public License as published by the Free 
- * Software Foundation, either version 3 of the License, or (at your option) 
- * any later version.
- *
- * "Commons" is distributed in the hope that it will be useful, but WITHOUT ANY 
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
- * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License 
- * along with "Commons". If not, see <http://www.gnu.org/licenses/>.
- */
+ * Copyright (c) 2012-2014, Andrea Funto'. All rights reserved. See LICENSE for details.
+ */ 
 package org.dihedron.patterns.experimental.pipeline;
 
-import static org.junit.Assert.fail;
-
-import org.dihedron.patterns.experimental.pipeline.Filter;
-import org.dihedron.patterns.experimental.pipeline.Link;
-import org.dihedron.patterns.experimental.pipeline.Producer;
 import org.dihedron.patterns.experimental.pipeline.impl.ArraySource;
 import org.dihedron.patterns.experimental.pipeline.impl.Log;
 import org.junit.Test;
