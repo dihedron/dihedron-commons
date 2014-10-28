dff0e1d76086bdadd15dc6cf0331e71a6cbaf84a
Andrea Funtò <dihedron.dev@gmail.com>
2014-10-28 14:22:21 +0100

    Added a random integer generator stream.

---
+++ src/main/java/org/dihedron/core/streams/RandomInputStream.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2012-2014, Andrea Funto'. All rights reserved. See LICENSE for details.
+ */ 
+package org.dihedron.core.streams;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
+
+/**
+ * An input stream returning a random sequence of non-negative integer values.
+ * 
+ * @author Andrea Funto'
+ */
+public class RandomInputStream extends InputStream {
+	
+	/**
+	 * A constant representing the lack of bounds to the generated random values.
+	 */
+	public static final int NO_LIMIT = -1;
+
+	/**
+	 * The internal random number generator.
+	 */
+	private Random random = null; 
+	
+	/**
+	 * The upper bound to generated random values (by default, no limit).
+	 */
+	private int limit = NO_LIMIT; 
+	
+	/**
+	 * Default constructor; initialises the internal random number generator using 
+	 * the current time as reported by #{@link java.lang.System#nanoTime()}.
+	 */
+	public RandomInputStream() {
+		this(System.nanoTime());
+	}
+	
+	/**
+	 * Constructor.
+	 * 
+	 * @param seed
+	 *   the seed used to initialise the internal random number generator.
+	 */
+	public RandomInputStream(long seed) {
+		this(seed, NO_LIMIT);		
+	}
+	
+	/**
+	 * Constructor.
+	 * 
+	 * @param seed
+	 *   the seed used to initialise the internal random number generator.
+	 * @param limit
+	 *   the upper bound (exclusive) for the generated integer value: if 50 is
+	 *   given here, the stream will generate numbers between 0 and 49 (included).
+	 */
+	public RandomInputStream(long seed, int limit) {
+		random = new Random(seed);
+		this.limit = limit;
+	}
+	
+
+	/**
+	 * @see java.io.InputStream#read()
+	 */
+	@Override
+	public int read() throws IOException {
+		return limit == NO_LIMIT ? random.nextInt() : random.nextInt(limit);
+	}
+}
