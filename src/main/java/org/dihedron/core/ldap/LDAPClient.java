e852cffa94f626e445941682020c8abec88f7135
Andrea Funtò <dihedron.dev@gmail.com>
2014-11-11 09:41:36 +0100

    Started working on LDAP client support.

---
+++ src/main/java/org/dihedron/core/ldap/LDAPClient.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2012-2014, Andrea Funto'. All rights reserved. See LICENSE for details.
+ */ 
+package org.dihedron.core.ldap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Andrea Funto'
+ */
+public class LDAPClient {
+	/**
+	 * The logger.
+	 */
+	private final static Logger logger = LoggerFactory.getLogger(LDAPClient.class);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
